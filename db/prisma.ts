9deb0392bda19361ff0103ef687de1331e3a5e7d
Mehedi Hasan Mahfuz <155963269+Lazy-BiT-Crazy@users.noreply.github.com>
2026-01-31 22:37:15 +0600

    Fix db/prisma.ts PrismaClient path

--- db/prisma.ts
+++ db/prisma.ts
@@ -1,6 +1,7 @@
 import { neonConfig } from "@neondatabase/serverless";
 import { PrismaNeon } from "@prisma/adapter-neon";
-import { PrismaClient } from "@prisma/client";
+
+import { PrismaClient } from "@/generated/prisma/client";
 import ws from "ws";
 
 neonConfig.webSocketConstructor = ws;
